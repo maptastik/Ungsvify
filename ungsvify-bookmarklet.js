166a2fc521ab3e6fabfb9f58d2734f4a57e1b7b1
Ryan Cooper <ryancooper729@gmail.com>
2016-08-18 00:33:16 -0400

    add ungsvify-bookmarklet.js

---
+++ ungsvify-bookmarklet.js
@@ -0,0 +1,1 @@
+javascript:(function()%7Bvar%20titleCard%20%3D%20%20document.getElementById(%22titlecard%22)%3BtitleCard.remove()%3Bvar%20viewcard%20%3D%20document.getElementsByClassName(%22app-viewcard-strip%22)%3Bviewcard%5B0%5D.remove()%3Bvar%20footer%20%3D%20document.getElementsByClassName(%22scene-footer-container%22)%3Bfooter%5B0%5D.remove()%7D)()
